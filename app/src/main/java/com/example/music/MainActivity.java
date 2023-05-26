0470322e8710828e21d4fd957e6ef1177584b33f
Sujan1714 <106664771+Sujan1714@users.noreply.github.com>
2023-05-26 14:55:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/music/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.music;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer mp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mp=MediaPlayer.create(this,R.raw.music);
+    }
+
+    public void str(View view) {
+        mp.start();
+    }
+
+    public void pau(View view) {
+        mp.pause();
+    }
+
+    public void stp(View view) {
+        mp.stop();
+    }
+}
